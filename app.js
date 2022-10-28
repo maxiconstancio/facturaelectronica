ffd64f677129d5d7a973e9ba1427f9d7656ac83d
maxiconstancio <maxiconstancio@gmail.com>
2022-10-28 20:38:38 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import { config } from 'dotenv';
+config();
+
+//import indexRouter from './apiServices/auth';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(path.resolve(), 'public')));
+
+//app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
+  export default app;
+  
