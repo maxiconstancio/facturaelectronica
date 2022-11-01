8edf85c68517694eef221e0e2f047cba6e0cf20f
maxiconstancio <maxiconstancio@gmail.com>
2022-10-31 21:25:24 -0300

    jwt verification

--- apiServices/auth/controller.js
+++ apiServices/auth/controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
-import jwt from "../utils/jwt.js";
-import Users from "../models/Users.js";
+import jwt from "../../services/jwt/index.js";
+import Users from "../../users/models.js";
 
 export async function login(req, res) {
     const { email, password } = req.body;
