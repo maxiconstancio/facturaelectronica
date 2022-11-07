e3645d597b0224d1a4ac1a7ae35363547aada83a
maxiconstancio <maxiconstancio@gmail.com>
2022-11-06 21:50:28 -0300

    add wsaa

--- bin/initialSetup.js
+++ bin/initialSetup.js
@@ -27,12 +27,8 @@ const findAdmin = async () => {
   });
   
   console.log(`new user created: ${newUser.email}`);
-  // const auth = await wsaa('wsfe');
-  const auth = {
-    Token: 'PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/Pgo8c3NvIHZlcnNpb249IjIuMCI+CiAgICA8aWQgc3JjPSJDTj13c2FhaG9tbywgTz1BRklQLCBDPUFSLCBTRVJJQUxOVU1CRVI9Q1VJVCAzMzY5MzQ1MDIzOSIgZHN0PSJDTj13c2ZlLCBPPUFGSVAsIEM9QVIiIHVuaXF1ZV9pZD0iMzEzMzYxNzQxNyIgZ2VuX3RpbWU9IjE2Njc3ODEzOTAiIGV4cF90aW1lPSIxNjY3ODI0NjUwIi8+CiAgICA8b3BlcmF0aW9uIHR5cGU9ImxvZ2luIiB2YWx1ZT0iZ3JhbnRlZCI+CiAgICAgICAgPGxvZ2luIGVudGl0eT0iMzM2OTM0NTAyMzkiIHNlcnZpY2U9IndzZmUiIHVpZD0iU0VSSUFMTlVNQkVSPUNVSVQgMjAzMzc3Mjk4MzYsIENOPXBydWViYSIgYXV0aG1ldGhvZD0iY21zIiByZWdtZXRob2Q9IjIyIj4KICAgICAgICAgICAgPHJlbGF0aW9ucz4KICAgICAgICAgICAgICAgIDxyZWxhdGlvbiBrZXk9IjIwMzM3NzI5ODM2IiByZWx0eXBlPSI0Ii8+CiAgICAgICAgICAgIDwvcmVsYXRpb25zPgogICAgICAgIDwvbG9naW4+CiAgICA8L29wZXJhdGlvbj4KPC9zc28+Cg==',
-    Sign: 'N4fWRg/dz4Kw/nYisJIR4UqvFDu8I0nj2XptBx9jtaHldPSNfUjl6CjkjLd3pNO75b0ijszRx6L7czykk+tPvvK9hoxHtwnJ3QSJFhfZiUXHLDCgkBHSUVy3oHnnC4o5q+8BGtZjzWDhqALhsxl7useHcnW+bSM5rxWZLBJhTbk=',
-    Cuit: '20337729836'
-  }
+  const auth = await wsaa('wsfe');
+
   const getComprobantes = await ListarComprobantes({Auth: auth});
 
   
