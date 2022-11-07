bafe02495d3db2826b6d76734a253880ea27045d
maxiconstancio <maxiconstancio@gmail.com>
2022-11-07 18:32:35 -0300

    authentication invoices

--- apiServices/invoices/routes.js
+++ apiServices/invoices/routes.js
@@ -2,19 +2,21 @@ import { Router } from 'express';
 import { getWsaa, registerInvoice, getAll } from './controllers.js';
 import { getInvoice } from '../../middleware/invoice.js';
 import { FECompUltimoAutorizado } from '../../services/afip/wsfe.js';
+import validJWT from '../../middleware/validJWT.js';
+import isAuthorized from '../../middleware/isAuthorized.js';
 
 const router = Router();
 
 // Get Authorization Token and Key
-router.get('/auth', getWsaa);
+router.get('/auth', validJWT, isAuthorized(2), getWsaa);
 
 //Get Autorizathion CAE and Register Invoice
-router.post('/invoice', getInvoice, registerInvoice)
+router.post('/invoice', validJWT, isAuthorized(2), getInvoice, registerInvoice)
 
 
 //Get All Invoices 
 
-router.get('/', getAll);
+router.get('/', validJWT, isAuthorized(2), getAll);
 
 
 
