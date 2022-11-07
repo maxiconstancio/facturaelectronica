25428064f273749c832bb6f6204e9a3afe3523db
maxiconstancio <84547967+maxiconstancio@users.noreply.github.com>
2022-11-07 18:37:58 -0300

    authentication JWT (#8)

--- apiServices/clients/routes.js
+++ apiServices/clients/routes.js
@@ -1,29 +1,31 @@
 import { Router } from 'express';
 
 import { getPersona, getWsaa } from '../../middleware/clients.js';  
+import isAuthorized from '../../middleware/isAuthorized.js';
+import validJWT from '../../middleware/validJWT.js';
 import {createClient, findClient, getAllClients, delClient, getStatus } from './controllers.js'
 
 const router = Router();
 
 //Gen WS Token and Key 
-router.get('/auth', getWsaa);
+router.get('/auth', validJWT, isAuthorized(2), getWsaa);
 
 //WS Dummy 
 
-router.get('/status', getStatus)
+router.get('/status',validJWT, isAuthorized(2), getStatus)
 
 //Get Client Information  and save
-router.post('/new', getPersona, createClient);
+router.post('/new', validJWT, isAuthorized(2), getPersona, createClient);
 
 //Get All Client
-router.get('/', getAllClients);
+router.get('/', validJWT, isAuthorized(2), getAllClients);
 
 //Get Client
-router.get('/find', findClient);
+router.get('/find', validJWT, isAuthorized(2), findClient);
 
 
 //delete Client
-router.delete('/:cuit', delClient);
+router.delete('/:cuit', validJWT, isAuthorized(2), delClient);
 
 
 
