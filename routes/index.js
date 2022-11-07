5ec6e6b0ad3d8a6977591818123f62c098e07e4c
maxiconstancio <maxiconstancio@gmail.com>
2022-11-06 22:25:27 -0300

    endpoint get typesDoc

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import invoices from '../apiServices/invoices/routes.js';
 import clients from '../apiServices/clients/routes.js';
+import typesDocs from '../apiServices/tiposDoc/routes.js'
 
 const router = Router();
 
 router.use('/invoices', invoices);
 router.use('/clients', clients)
+router.use('/typesdocs', typesDocs)
 
 
 export default router;
