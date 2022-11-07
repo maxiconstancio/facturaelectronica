609f7a658a627c54737eb7185f1ae835dc7e0d94
maxiconstancio <maxiconstancio@gmail.com>
2022-11-07 18:35:00 -0300

    authentication endpoint tiposDoc

--- apiServices/tiposDoc/routes.js
+++ apiServices/tiposDoc/routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import validJWT from '../../middleware/validJWT.js';
 import { getAll } from './controllers.js';
 
 const router = Router();
 
 
-router.get('/', getAll)
+router.get('/', validJWT, isAuthorized(2), getAll)
 
 export default router;
