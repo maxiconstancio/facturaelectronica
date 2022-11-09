77c7681aa935d76fc56d57b53325924aecad5701
maxiconstancio <84547967+maxiconstancio@users.noreply.github.com>
2022-11-08 21:05:25 -0300

    Datauser (#12)
    * add dataUser, client info to invoices
    
    * fix issues

--- apiServices/invoices/controllers.js
+++ apiServices/invoices/controllers.js
@@ -1,5 +1,6 @@
 import wsaa from '../../services/afip/wsaa.js';
 import Invoice from './model.js';
+import Clients from "../clients/model.js";
 
 export const getWsaa = async (req, res, next) =>  {
     try {
@@ -13,7 +14,11 @@ export const getWsaa = async (req, res, next) =>  {
 export const registerInvoice = async (req, res) => {
 
     try {
+        const env = process.env;
         const data = req.data
+        const dataUser = {razonSocial: env.RAZON_SOCIAL, Cuit: env.CUIT, domicilio: env.DOMICILIO, iva: env.IVA, IIBB: env.IIBB, inicio: env.INICIO_ACTIVIDAD }
+        const client = await Clients.findOne({cuit: data.DocNro});
+        
         const newInvoice = await Invoice.create({
             concepto: data.Concepto,
             docTipo: data.DocTipo,
@@ -27,8 +32,20 @@ export const registerInvoice = async (req, res) => {
             detalle: req.body.detalle,
             CAE: req.CAE
         })
-               
-        return res.status(200).json(newInvoice)
+        
+        const invoice = {
+            concepto: newInvoice.concepto,
+            cbteTipo: newInvoice.cbteTipo,
+            ptoVenta: newInvoice.ptoVenta,
+            comprobante:  newInvoice.comprobante,
+            fechaComprobante: newInvoice.fechaComprobante,
+            importeNeto: newInvoice.importeNeto,
+            importeTotal: newInvoice.importeTotal,
+            detalle: newInvoice.detalle,
+            CAE:  newInvoice.CAE
+        }
+        
+        return res.status(200).json({dataUser, client, invoice})
         
        //return res.status(200).json(data)
     } catch (error) {
